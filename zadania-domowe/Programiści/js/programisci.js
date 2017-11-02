2c9abc3d7cbcd39cff1f04ecc16c23c56a9c3d4a
Aleksandra Kołodziejczak <aleksandra@kolodziejczak.info>
2017-11-02 13:53:50 +0100

    commit

--- zadania-domowe/Programiści/js/programisci.js
+++ zadania-domowe/Programiści/js/programisci.js
@@ -2,16 +2,12 @@
 
 $(document).ready(function () {
     
-    $('button').after("<div id='dane-programisty'</div>");
-
-
-    $("button").click(pobierzDane);
-
-    function pobierzDane(options) {
+    $('button').after("<div id='dane-programisty'></div>");
+    
+    $("button").click(function() {
         $.getJSON("https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php", function (data) {
-            console.log(data);
-
-            document.getElementById('dane-programisty').innerHTML = "imie: " + data.imie + ", " + "nazwisko: " + data.nazwisko + ", " + "zawód: " + data.zawod + ", " + "firma: " + data.firma;
-        });
-    };
+           console.log(data);
+       document.getElementById('dane-programisty').innerHTML = "imie: " + data.imie + ", " + "nazwisko: " + data.nazwisko + ", " + "zawód: " + data.zawod + ", " + "firma: " + data.firma;
+       });
+    });   
 });
