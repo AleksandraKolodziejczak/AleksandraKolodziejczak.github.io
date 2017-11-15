8516a24a652758619b9ea1eef51e6b670bae3430
Aleksandra Kołodziejczak <aleksandra@kolodziejczak.info>
2017-11-15 19:37:03 +0100

    kolejny commit

--- Praca-domowa/js/ajax.js
+++ Praca-domowa/js/ajax.js
@@ -1,22 +1,41 @@
 $(document).ready(function () {
+
+
     $("button").click(pobierzDane);
+
 
     function pobierzDane(options) {
         $.getJSON("https://blockchain.info/pl/ticker", function (data) {
             console.log(data.PLN.sell);
 
+            document.getElementById('buy').innerHTML = "buy: " + data.PLN.buy;
             document.getElementById('sell').innerHTML = "sell: " + data.PLN.sell;
-            document.getElementById('last').innerHTML = "last: " + data.PLN.last;
+
 
 
             if (data.PLN.sell > data.PLN.last) {
-                ($('#result').html("<i class='fa fa-arrow-up' aria-hidden='true'></i>"));
+
+
+                ($('#sell').append("<i class='fa fa-arrow-up' aria-hidden='true'></i>"));
             } else if (data.PLN.sell == data.PLN.last) {
-                ($('#result').html("<i class='fa fa-arrows-h' aria-hidden='true'></i>"));
+                ($('#sell').append("<i class='fa fa-arrows-h' aria-hidden='true'></i>"));
             } else {
-                ($('#result').html("<i class='fa fa-arrow-down' aria-hidden='true'></i>"));
+                ($('#sell').append("<i class='fa fa-arrow-down' aria-hidden='true'></i>"));
+            }
+
+
+
+            if (data.PLN.buy > data.PLN.last) {
+
+
+                ($('#buy').append("<i class='fa fa-arrow-up' aria-hidden='true'></i>"));
+            } else if (data.PLN.buy == data.PLN.last) {
+                ($('#buy').append("<i class='fa fa-arrows-h' aria-hidden='true'></i>"));
+            } else {
+                ($('#buy').append("<i class='fa fa-arrow-down' aria-hidden='true'></i>"));
             }
 
         });
+
     }
 });
